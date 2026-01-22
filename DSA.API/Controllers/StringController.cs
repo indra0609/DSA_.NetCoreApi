bbc00d7f077d813fa9ce689152d983c5a71013a9
indra0609 <indra76ju@gmail.com>
2026-01-22 22:40:54 +0530

    ReverseString

---
+++ DSA.API/Controllers/StringController.cs
@@ -0,0 +1,25 @@
+﻿using DSA.String;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSA.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StringController : ControllerBase
+    {
+        #region EasyString
+
+        EasyString easyString = new EasyString();
+
+        #region ReverseString
+        [HttpPost("ReverseString")]
+        public IActionResult ReverseString([FromBody] string s)
+        {
+            return Ok(easyString.ReverseString(s.ToCharArray()));
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ DSA.String/EasyString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.String
+{
+    public class EasyString
+    {
+        #region ReverseString
+        public string ReverseString(char[] s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            while (i <= j)
+            {
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+                i++;
+                j--;
+            }
+            return new string(s);
+        }
+
+        #endregion
+    }
+}
