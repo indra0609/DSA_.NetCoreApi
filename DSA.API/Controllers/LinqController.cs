994c63690f718ff3f8e7afa98bcd9a0b9628f028
indra0609 <indra76ju@gmail.com>
2026-02-26 00:12:23 +0530

    add Linq

---
+++ DSA.API/Controllers/LinqController.cs
@@ -0,0 +1,57 @@
+﻿using DSA.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSA.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqController : ControllerBase
+    {
+        EasyLinq easyLinq = new EasyLinq();
+
+        #region Easy
+        [HttpPost("GetEven")]
+        public IActionResult GetEven([FromBody] int[] arr)
+        {
+            var result = easyLinq.GetEven(arr);
+            return Ok(result);
+        }
+
+        [HttpPost("GetGreaterThan")]
+        public IActionResult GetGreaterThan([FromBody] int[] arr, int value)
+        {
+            var result = easyLinq.GetGreaterThan(arr, value);
+            return Ok(result);
+        }
+
+        [HttpPost("GetDistinct")]
+        public IActionResult GetDistinct([FromBody] int[] arr)
+        {
+            var result = easyLinq.GetDistinct(arr);
+            return Ok(result);
+        }
+
+        [HttpPost("CountElements")]
+        public IActionResult CountElements([FromBody] int[] arr)
+        {
+            var count = easyLinq.CountElements(arr);
+            return Ok(count);
+        }
+
+        [HttpPost("Contains")]
+        public IActionResult Contains([FromBody] int[] arr, int n)
+        {
+            var res = easyLinq.Contains(arr, n);
+            return Ok(res);
+        }
+
+        [HttpPost("GetDuplicates")]
+        public IActionResult GetDuplicates([FromBody] int[] arr)
+        {
+            var duplicates = easyLinq.GetDuplicates(arr);
+            return Ok(duplicates);
+        }
+        #endregion
+    }
+}
---
+++ DSA.Linq/EasyLinq.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Linq
+{
+    public class EasyLinq
+    {
+        #region Filter even numbers from a list
+        public int[] GetEven(int[] arr)
+        {
+            var result = arr.Where(x => x % 2 == 0).ToArray();
+            return result;
+        }
+        #endregion
+
+        #region Find numbers greater than a given value
+        public int[] GetGreaterThan(int[] arr, int value)
+        {
+            var result = arr.Where(x => x > value).ToArray();
+            return result;
+        }
+        #endregion
+
+        #region Get distinct elements from a list
+        public int[] GetDistinct(int[] arr)
+        {
+            var result = arr.Distinct().ToArray();
+            return result;
+        }
+        #endregion
+
+        #region Count total elements in a list
+        public int CountElements(int[] arr)
+        {
+            var count = arr.Count();
+            return count;
+        }
+        #endregion
+
+        #region Check if a value exists in a list (`Any`)
+        public bool Contains(int[] arr, int n)
+        {
+             var res = arr.Any(x => x == n);
+
+             return res;
+        }
+
+        #endregion
+
+        #region Find duplicate elements  ***
+        public int[] GetDuplicates(int[] arr)
+        {
+            var duplicates = arr.GroupBy(x => x)
+                                .Where(g => g.Count() > 1)
+                                .Select(g => g.Key)
+                                .ToArray();
+            return duplicates;
+        }
+
+        #endregion
+
+    }
+}
